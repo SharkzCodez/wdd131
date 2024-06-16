385d70c467cd13060ddfe9e4de79926337c4bf0d
SharkzCodez <z2leblanc@outlook.com>
2024-06-16 14:39:08 -0500

    w1 assignment

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,6 @@
+//const currentDate = document.querySelector("#currentyear");
+//const lastEdit = document.querySelector("#lastModified");
+
+//const timeNow = new Date(); console.log(timeNow); 
+
+//let lastEdit = new Date(document.lastModified);
