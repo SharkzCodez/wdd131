2e2b474bd63ba09be5369df2aaff801488c24ab2
SharkzCodez <z2leblanc@outlook.com>
2024-07-05 14:21:40 -0500

    W2 Pictures

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+document.querySelector('#currentyear').textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = new Date();
+
+const today = new Date();
+
+//let lastModified = new Date(document.lastModified);
+lastModified.innerHTML = `Last modified: ${new Intl.DateTimeFormat("en-US",
+    {
+        dateStyle: "lastModified"
+
+    }
+).format(today)}`;
